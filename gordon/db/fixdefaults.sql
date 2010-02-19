67cd9669b2e7c2215a363ad198e3e468a6e62555
Douglas Eck <douglas.eck@gmail.com>
2010-02-18 19:29:05 -0500

    added new sql commands

--- gordon/db/fixdefaults.sql
+++ gordon/db/fixdefaults.sql
@@ -1,3 +1,4 @@
+/* 
 alter table track alter column mb_id set default '';
 alter table track alter column path set default '';
 alter table track alter column title set default '';
@@ -15,4 +16,11 @@ alter table track alter column ofilename set default '';
 alter table track alter column otracknum set default -1;
 alter table track alter column source set default '';
 alter table track alter column bytes set default -1;
- 
+*/ 
+alter table album_stats rename to album_status;
+alter index album_stats_pkey rename to album_status_pkey;
+alter relation album_stats_album_id_fkey rename to album_status_album_id_fkey;
+alter index album_stats_album_id_fkey rename to album_status_album_id_fkey;
+alter table album_status drop constraint album_stats_album_id_fkey;
+alter sequence album_stats_id_seq rename to album_status_id_seq;
+
