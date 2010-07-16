451a90c11df8c04306f23a3757ff104b900b3ee4
Ron Weiss <ronweiss@gmail.com>
2010-07-16 18:11:21 -0400

    feature extraction caching bug fixes

--- example_feature_extractors/simple/rms_energy.py
+++ example_feature_extractors/simple/rms_energy.py
@@ -3,4 +3,4 @@ import numpy as np
 def extract_features(track):
    """Computes the RMS energy in dB of the entire track."""
    samples, fs, svals = track.audio()
-   return 20*np.log10(np.sqrt(np.mean(samples**2)))
+   return [20*np.log10(np.sqrt(np.mean(samples**2)))]
